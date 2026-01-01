4069e087c6d5611b1930a81848d6cd39b663a471
Haider Alshamma <haideralsh@gmail.com>
2025-12-31 22:47:17 -0500

    Initial Commit

---
+++ lime/limeApp.swift
@@ -0,0 +1,17 @@
+//
+//  limeApp.swift
+//  lime
+//
+//  Created by Haider on 2025-12-31.
+//
+
+import SwiftUI
+
+@main
+struct limeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ limeTests/limeTests.swift
@@ -0,0 +1,16 @@
+//
+//  limeTests.swift
+//  limeTests
+//
+//  Created by Haider on 2025-12-31.
+//
+
+import Testing
+
+struct limeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
